ccacd7a6b13a8cb6d382b088c3f24d64d116a168
reiamenezes2004 <RM1661@live.mdx.ac.uk>
2024-02-20 09:15:38 +0400

    Add files via upload

---
+++ products.js
@@ -0,0 +1,48 @@
+let products = [
+    {
+        "id":1001,
+        "title": "Dog food, 25lb bag",
+        "description": "A 25 pound bag of <em>irresitible</en>, organic goodness for your dog.",
+        "price": 20.00,
+        "image": "images/dogfood.png",
+        "availableInventory": 10,
+        "rating": 2
+    },
+    {
+        "id":1002,
+        "title": "Fish food, 25lb bag",
+        "description": "A 25 pound bag of <em>irresitible</en>, organic goodness for your fish.",
+        "price": 15.00,
+        "image":"images/fishfood.jpeg",
+        "availableInventory": 10,
+        "rating": 2
+    },
+    {
+        "id":1003,
+        "title": "Cat food, 25lb bag",
+        "description": "A 25 pound bag of <em>irresitible</en>, organic goodness for your cat.",
+        "price": 30.00,
+        "image":"images/catfood.png",
+        "availableInventory": 10,
+        "rating": 2
+    },
+    {
+        "id":1004,
+        "title": "Bird food, 25lb bag",
+        "description": "A 25 pound bag of <em>irresitible</en>, organic goodness for your bird.",
+        "price": 25.00,
+        "image":"images/birdfood.png",
+        "availableInventory": 10,
+        "rating": 2
+    },
+    {
+        "id":1005,
+        "title": "Rabbit food, 25lb bag",
+        "description": "A 25 pound bag of <em>irresitible</en>, organic goodness for your rabbit.",
+        "price": 10.00,
+        "image":"images/rabbitfood.png",
+        "availableInventory": 10,
+        "rating": 2
+    },
+
+]
