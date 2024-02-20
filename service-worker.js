8b29c538ef6d225467be18e8e9171cfef39ea486
reiamenezes2004 <RM1661@live.mdx.ac.uk>
2024-02-20 10:01:18 +0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -25,8 +25,13 @@ self.addEventListener('install', (e) => {
 self.addEventListener('fetch', function (e){
     e.respondWith(
         caches.match(e.request).then(function (r){
-            console.log('[Service Worker] Fetching resource: ' + e.request.url);
-            return r
+            // console.log('[Service Worker] Fetching resource: ' + e.request.url);
+            return r || fetch(e.request).then(function(response){
+                return caches.open(cacheName).then(function (cache){
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            });
         })
     );
 });
